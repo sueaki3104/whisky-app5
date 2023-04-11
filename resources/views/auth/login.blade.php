305520392f9f224a8d3d335f95f64d556eb4a76d
sueaki3104 <sueaki@a-omega.co.jp>
2023-04-11 14:08:37 +0900

    最初のPOPUPのradioからBUTTONni

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,13 +24,13 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
+        <!-- Remember Me ここは実装しない-->
+        <!-- <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ml-2 text-sm text-gray-600">{{ __('覚えておく') }}</span>
             </label>
-        </div>
+        </div> -->
 
         <div class="flex items-center justify-end mt-4">
             <div class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -39,11 +39,12 @@
 
             <div class="ml-3"></div> <!-- 余白を追加 -->
 
-            @if (Route::has('password.request'))
+            <!-- ここも今は実装しない -->
+            <!-- @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('パスワードを忘れた') }}
                 </a>
-            @endif
+            @endif -->
 
             <x-primary-button class="ml-3">
                 {{ __('ログイン') }}
