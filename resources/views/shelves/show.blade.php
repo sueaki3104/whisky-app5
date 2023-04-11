3d2af6f8ee81cd42e9eee225b5af398c9a340d06
sueaki3104 <sueaki@a-omega.co.jp>
2023-04-11 15:55:54 +0900

    shelvesno

--- resources/views/shelves/show.blade.php
+++ resources/views/shelves/show.blade.php
@@ -63,7 +63,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ Auth::user()->name.__('のウィスキー') }}
+            {{ Auth::user()->name.__('さんのウィスキー詳細') }}
         </h2>
     </x-slot>
 
