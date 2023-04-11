b5aa6edf6c0d5fddc392642954a8158459f1cbd4
sueaki3104 <sueaki@a-omega.co.jp>
2023-04-11 09:35:21 +0900

    ボトル新規登録に説明文追加

--- resources/views/shelves/new-bottle.blade.php
+++ resources/views/shelves/new-bottle.blade.php
@@ -17,35 +17,35 @@
 
                     <!-- 購入日 -->
                     <div class="mt-4">
-                        <x-input-label for="buy_date" :value="__('購入日')" />
+                        <x-input-label for="buy_date" :value="__('購入日（必須）')" />
                         <x-text-input id="buy_date" class="block mt-1 w-full" type="date" name="buy_date" :value="old('buy_date')" required />
                         <x-input-error :messages="$errors->get('buy_date')" class="mt-2" />
                     </div>
 
                     <!-- 購入場所 -->
                     <div class="mt-4">
-                        <x-input-label for="buy_address" :value="__('購入場所')" />
+                        <x-input-label for="buy_address" :value="__('購入場所（必須）')" />
                         <x-text-input id="buy_address" class="block mt-1 w-full" type="text" name="buy_address" :value="old('buy_address')" required />
                         <x-input-error :messages="$errors->get('buy_address')" class="mt-2" />
                     </div>
 
                     <!-- ウイスキー名 -->
                     <div class="mt-4">
-                        <x-input-label for="name" :value="__('ウイスキーの名前')" />
+                        <x-input-label for="name" :value="__('ウイスキーの名前（必須）')" />
                         <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <!-- 単価 -->
                     <div class="mt-4">
-                        <x-input-label for="price" :value="__('単価(税込)')" />
+                        <x-input-label for="price" :value="__('単価(税込)（必須）')" />
                         <x-text-input id="price" class="block mt-1 w-full" type="number" name="price" :value="old('price')" required />
                         <x-input-error :messages="$errors->get('price')" class="mt-2" />
                     </div>
 
                     <!-- 購入数量 -->
                     <div class="mt-4">
-                        <x-input-label for="num" :value="__('購入数量(半角数字のみ)')" />
+                        <x-input-label for="num" :value="__('購入数量(半角数字のみ)（必須）')" />
                         <x-text-input id="num" class="block mt-1 w-full" type="number" name="num" :value="old('num')" required />
                         <x-input-error :messages="$errors->get('num')" class="mt-2" />
                     </div>
@@ -53,15 +53,18 @@
 
                     <!-- 思い出 -->
                     <div class="flex flex-col mt-4">
-                        <x-input-label for="memory" :value="__('思い出（テキスト191文字）')" />
+                        <x-input-label for="memory" :value="__('思い出（テキスト191文字）（必須）')" />
                         <textarea class="border py-2 px-3 text-grey-darkest resize-none" name="memory" id="memory" rows="4"></textarea>
                     </div>
 
                     <!-- 写真 -->
-                    <x-shelves.form.images></x-shelves.form.images>
+                    <div class="flex flex-col mt-4">
+                        <x-input-label for="photo" :value="__('画像MAX1枚（ない場合は、当サイトのロゴが入ります）')" />
+                        <x-shelves.form.images></x-shelves.form.images>
+                    </div>
 
                     <button type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
-                        投稿する
+                        登録する
                     </button>
                 </form>
 
